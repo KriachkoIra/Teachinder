741adec0e1ac4fe80127c17ebd9bcb82af0d3e2f
KriachkoIra <kriachkoira@gmail.com>
2022-12-04 22:23:11 +0100

    lab1 done

--- scripts/teacherInfo.js
+++
@@ -1,9 +0,0 @@
-function openInfo() {
-  document.getElementById("info").style.display = "block";
-    document.getElementById("bg").style.display = "block";
-}
-
-function closeInfo() {
-  document.getElementById("info").style.display = "none";
-  document.getElementById("bg").style.display = "none";
-}
