bc906492547eb42263c311506003508fe67c0a1a
KriachkoIra <kriachkoira@gmail.com>
2022-12-04 22:29:35 +0100

    lab3 done

--- scripts/openMap.js
+++
@@ -1,9 +0,0 @@
-function openMap() {
-    if(document.getElementById("teacherMap").style.display === 'none'){
-        document.getElementById("teacherMap").style.display = "block";
-        document.getElementById("info").style.height = "700px";
-    } else{
-        document.getElementById("teacherMap").style.display = "none";
-        document.getElementById("info").style.height = "470px";
-    }
-}
--- scripts/popup.js
+++
@@ -1,9 +0,0 @@
-function openForm() {
-  document.getElementById("myForm").style.display = "block";
-    document.getElementById("bg").style.display = "block";
-}
-
-function closeForm() {
-  document.getElementById("myForm").style.display = "none";
-  document.getElementById("bg").style.display = "none";
-}
