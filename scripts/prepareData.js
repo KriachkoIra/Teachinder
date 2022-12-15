3f7bbe265a450994d61cef48bc44ca8aeb96579f
KriachkoIra <kriachkoira@gmail.com>
2022-12-15 01:54:54 +0100

    lab5.1 done

--- scripts/prepareData.js
+++ scripts/prepareData.js
@@ -1,17 +1,46 @@
-import { randomUserMock } from "./FE4U-Lab3-mock.js";
+//import { randomUserMock } from "./FE4U-Lab3-mock.js";
 import { additionalUsers } from "./FE4U-Lab3-mock.js";
 
 var txt = "";
+var xhttp = new XMLHttpRequest();
+var randomUserMock = {};
 
 export const teachers = loadTeachers();
 //teachers.forEach(func1);
 //document.getElementById("test").innerHTML = txt;
 
-function func1(myObj) {
-  txt += myObj.id + ". " + myObj.full_name + "<br>";
+function httpGet(n){
+    return new Promise(function(resolve, reject){
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', "https://randomuser.me/api/?results=" + n, true);
+        
+        xhr.onload = function(){
+            if(this.status == 200){
+                resolve(this.response);
+            } else{
+                var error = new Error(this.statusText);
+                error.code = this.status;
+                reject(error);
+            }
+        };
+        
+        xhr.onerror = function(){
+            reject(new Error("Network Error"));
+        };
+        
+        xhr.send();
+    });
+}
+
+function getTeachers(n) {
+  xhttp.open("GET", "https://randomuser.me/api/?results="+n, false);
+    xhttp.send();
+    var text = xhttp.responseText;
+    randomUserMock = JSON.parse(xhttp.response).results;
 }
 
 function loadTeachers() {
+    getTeachers(50);
     let people = [];
     let courses = ["Mathematics", "Physics", "English", "Computer Science", "Dancing", "Chess", "Biology", "Chemistry", "Law", "Art", "Medicine", "Statistics"];
     
@@ -39,57 +68,6 @@ function loadTeachers() {
         person.picture_thumbnail = randomUserMock[i].picture.thumbnail;
         
         people.push(person); // add a created person to the array
-    }
-    
-    // add additional users
-    for (let i = 0; i < additionalUsers.length; i++) {
-        let check = people.find(pers => pers.full_name == additionalUsers[i].full_name); // check if person exists
-        if(!check){
-            console.log("Person already exists. " + additionalUsers[i].full_name);
-            continue;
-        } 
-        
-        let person = {};
-        person.id = people.length; // id
-        if(!additionalUsers[i].favorite)
-            person.favorite = Math.random() < 0.5; // add favorite - random boolean
-        else person.favorite = additionalUsers[i].favorite;
-        
-        if(!additionalUsers[i].course)
-            person.course = courses[Math.floor(Math.random() * 12)]; // random course from the array
-        else person.course = additionalUsers[i].course;
-        
-        if(!additionalUsers[i].bgcolor)
-        person.bgcolor = "#" + Math.floor(Math.random()*16777215).toString(16); // random bgcolor
-        else person.bgcolor = additionalUsers[i].bgcolor;
-        
-        if(!additionalUsers[i].note)
-        person.note = "No notes yet."; // note
-        else person.note = additionalUsers[i].note;
-        
-        person.gender = additionalUsers[i].gender;
-        person.title = additionalUsers[i].title;
-        person.full_name = additionalUsers[i].full_name;
-        person.city = additionalUsers[i].city;
-        person.state = additionalUsers[i].state;
-        person.country = additionalUsers[i].country;
-        person.postcode = additionalUsers[i].postcode;
-        person.coordinates = additionalUsers[i].coordinates;
-        person.timezone = additionalUsers[i].timezone;
-        person.email = additionalUsers[i].email;
-        person.b_date = additionalUsers[i].b_date;
-        person.age = additionalUsers[i].age;
-        person.phone = additionalUsers[i].phone;
-        person.picture_large = additionalUsers[i].picture_large;
-        person.picture_thumbnail = additionalUsers[i].picture_thumbnail;
-        
-        const keys = Object.keys(person);
-        for (let i = 0; i < keys.length; i++) {
-          if(typeof person[keys[i]] == 'undefined')
-              person[keys[i]] = null;
-        }
-        
-        people.push(person);
     }
     
     return people;
